d1586152a41cbecf4c6f7deb281702936d00e41e
Denis-Bez <v417459@yandex.ru>
2022-03-30 19:01:16 +0500

    pp001

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\v4174\git\MyRepose\PP01_Site_Expertise\.venv
+set VIRTUAL_ENV=C:\Users\v4174\git\MyRepose\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- app.py
+++ app.py
@@ -1,10 +1,55 @@
+from config import CONFIG
+
 from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 
 app = Flask (__name__)
 
-@app.route("/")
+app.config["MAIL_DEFAULT_SENDER"] = CONFIG["MAIL_DEFAULT_SENDER"]
+app.config["MAIL_PASSWORD"] = CONFIG["MAIL_PASSWORD"]
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = CONFIG["MAIL_USERNAME"]
+mail = Mail(app)
+
+# Main html
+@app.route("/", methods=["POST", "GET"])
 def index():
+
+    if request.method == "POST":
+        msg = Message("Hey", recipients=['v417459@yandex.ru'])
+        msg.body = ("Hey how are you& Is everything okay&")
+        mail.send(msg)
+
     return render_template("index.html")
+
+@app.route("/services")
+def services():
+
+    return render_template("services.html")
+
+@app.route("/experts")
+def experts():
+
+    return render_template("experts.html")
+
+@app.route("/contacts")
+def contacts():
+
+    return render_template("contacts.html")
+
+# Invisible html
+@app.route("/email")
+def email():
+
+    msg = Message("Hey", recipients=['v417459@yandex.ru'])
+    msg.body = ("Hey how are you& Is everything okay&")
+    mail.send(msg)
+
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
