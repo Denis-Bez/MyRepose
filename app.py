0b1dd190602bb1289c896d53b20bb1d1b122b2cb
Denis-Bez <v417459@yandex.ru>
2022-03-31 20:48:18 +0500

    pp001

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from config import CONFIG
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request
 from flask_mail import Mail, Message
 
 app = Flask (__name__)
+
+title = {
+    "index": "ЭНЕРГО group Экспертиза - Энергия в руках профессионалов",
+    "services": "Заказать экспертизу электротехнических работ и оборудования" 
+}
 
 app.config["MAIL_DEFAULT_SENDER"] = CONFIG["MAIL_DEFAULT_SENDER"]
 app.config["MAIL_PASSWORD"] = CONFIG["MAIL_PASSWORD"]
@@ -15,15 +20,17 @@ app.config["MAIL_USERNAME"] = CONFIG["MAIL_USERNAME"]
 mail = Mail(app)
 
 # Main html
-@app.route("/", methods=["POST", "GET"])
+@app.route("/") #methods=["POST", "GET"]
 def index():
 
-    if request.method == "POST":
-        msg = Message("Hey", recipients=['v417459@yandex.ru'])
-        msg.body = ("Hey how are you& Is everything okay&")
-        mail.send(msg)
+#    if request.method == "POST":
+#        msg = Message("Hey", recipients=['v417459@yandex.ru'])
+#        msg.body = ("Hey how are you& Is everything okay&")
+#        mail.send(msg)
 
-    return render_template("index.html")
+    #return render_template("services.html")
+
+    return render_template("index.html", title=title["index"])
 
 @app.route("/services")
 def services():
